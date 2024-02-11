e5dfb1b98eb774b9ce473174adf74919c6d5540d
aasemgar7i <aasemgar7i@gamil.com>
2024-02-11 22:59:18 +0200

    py

--- base_model.py
+++ base_model.py
@@ -25,27 +25,31 @@ class BaseModel:
 
         Args:
             *args: Unused arguments.
-            **kwargs: Keyword arguments representing attribute name-value pairs.
-                      Each key is an attribute name, and each value is the value of that attribute.
-                      If 'kwargs' is not empty, it recreates the instance from the dictionary representation.
-                      Otherwise, it creates a new instance with generated 'id' and 'created_at'.
+            **kwargs: Keyword arguments representing
+                      attribute name-value pairs.
+                      Each key is an attribute name, and each value is
+                      the value of that attribute.
+                      If 'kwargs' is not empty, it recreates the instance
+                      from the dictionary representation.
+                      Otherwise, it creates a new instance
+                      with generated 'id' and 'created_at'.
 
         Note:
-            'created_at' and 'updated_at' attributes are converted from string to datetime objects.
+            'created_at' and 'updated_at' attributes are converted
+             from string to datetime objects.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
+            models.storage.new(self)
 
     def save(self):
         """
@@ -53,7 +57,7 @@ class BaseModel:
         and save the instance to the storage.
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -75,4 +79,5 @@ class BaseModel:
         Returns:
             str: String representation of the instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
